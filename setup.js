81c6095e89fd9146514e3ffff60b698e490bf85a
srohatgi <sumeet_rohatgi@hotmail.com>
2012-03-15 00:35:06 -0700

    added REDIS_SERVER_LOC

--- setup.js
+++ setup.js
@@ -12,7 +12,8 @@ var push_js = path.join(__dirname, 'push.js'),
     push_port_range = process.env.PORT_RANGE_PUSH || '3000',
     push_children = process.env.SPAWN_PUSH || '1',
     redis_conf_loc = process.env.REDIS_CONF_FILE || '/Users/sumeet/Downloads/redis-2.4.8/redis.conf',
-    push_cluster_uri = process.env.PUSH_CLUSTER_URI || 'localhost:3000';
+    push_cluster_uri = process.env.PUSH_CLUSTER_URI || 'localhost:3000',
+    redis_server_loc = process.env.REDIS_SERVER_LOC || '/usr/local/bin/redis-server';
 
 // client config
 var forever_exe = path.join(__dirname, '/node_modules/forever/bin/forever')
@@ -23,7 +24,7 @@ var stop_script = rundir+'/stop.sh';
 var client_script = rundir+'/cli.sh';
 
 // datastore - redis start
-spawn_datastore(rundir,publish_db,device_db,redis_conf_loc,start_script,stop_script);
+spawn_datastore(redis_server_loc,rundir,publish_db,device_db,redis_conf_loc,start_script,stop_script);
 // push - node.js start
 spawn_push(push_js,rundir,push_children,push_port_range,publish_db,device_db,percent_collab,start_script,stop_script);
 // publish - node.js script
@@ -31,15 +32,15 @@ trigger_publisher(publish_js,rundir,device_db,publish_db,publish_interval,start_
 // do run cleanup
 run_cleanup(rundir,stop_script);
 // client - java program
-trigger_client(rundir,client_script);
+trigger_client(rundir,push_cluster_uri,client_script);
 
-function spawn_datastore(rundir,publish_addr,device_addr,redis_conf_loc,start_script,stop_script) {
+function spawn_datastore(redis_server_loc,rundir,publish_addr,device_addr,redis_conf_loc,start_script,stop_script) {
   var port = publish_addr.substring(publish_addr.indexOf(':')+1);
   var publishdb_conf = rundir+'/publishdb.conf';
   var publishdb_pid = rundir+'/publishdb.pid';
   util.puts(util.format('sed -e "s#pidfile /var/run/redis.pid#pidfile %s#g" -e "s/6379/%s/g" -e "s#dir \./#dir %s#g" -e "s/daemonize no/daemonize yes/g" -e "s#logfile stdout#logfile %s/publishdb.log#g" %s > %s'
               ,publishdb_pid,port,rundir,rundir,redis_conf_loc,publishdb_conf));
-  util.puts(util.format('echo redis-server %s >> %s',publishdb_conf,start_script));
+  util.puts(util.format('echo %s %s >> %s',redis_server_loc,publishdb_conf,start_script));
   util.puts(util.format('echo kill -9 \\`cat %s\\` >> %s',publishdb_pid,stop_script));
   
   if ( publish_addr !== device_addr ) {
@@ -78,7 +79,7 @@ function run_cleanup(rundir,stop_script) {
   util.puts(util.format('echo rm -f %s/\*.log %s/\*.pid %s/\*.rdb >> %s',rundir,rundir,rundir,stop_script));
 }
 
-function trigger_client(rundir,client_script) {
+function trigger_client(rundir,push_cluster_uri,client_script) {
   var ws_jar = path.join(__dirname, 'client/lib/WebSocket.jar');
   var cm_jar = path.join(__dirname, 'client/lib/commons-math-2.2.jar');
   var json_jar = path.join(__dirname, 'client/lib/gson-2.1.jar');
