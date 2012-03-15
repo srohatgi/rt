ebc533721f5e1a09dd1761f30494469a751ec901
srohatgi <sumeet_rohatgi@hotmail.com>
2012-03-15 00:25:43 -0700

    added debugging symbols

--- client/build.sh
+++ client/build.sh
@@ -1,2 +1,2 @@
 rm -f classes/*.class
-javac -classpath lib/WebSocket.jar:lib/commons-math-2.2.jar:lib/gson-2.1.jar -d classes SocketIOClient.java LoadTester.java SocketIOClientEventListener.java
+javac -g -classpath lib/WebSocket.jar:lib/commons-math-2.2.jar:lib/gson-2.1.jar -d classes SocketIOClient.java LoadTester.java SocketIOClientEventListener.java
